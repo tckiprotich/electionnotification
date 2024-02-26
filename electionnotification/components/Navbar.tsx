7017ad692fcd3cc90b4059cefa2ade5ffbce2d33
COLLINS TONUI <tckiprotich@outlook.com>
2024-02-26 08:34:17 +0000

    Update MongoDB URI and Navbar link

--- electionnotification/components/Navbar.tsx
+++ electionnotification/components/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
             <nav className="hidden gap-12 lg:flex">
               <a href="/" className="text-lg font-semibold text-indigo-500">Home</a>
               <a href="/admin" className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700">Admin</a>
-              <a href="upload" className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700">Upload</a>
+              <a href="contacts" className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700">Upload</a>
               <a href="list" className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700">List</a>
             </nav>  
             <a href="/new" className="hidden rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:inline-block ml-10">create campaign</a>         
